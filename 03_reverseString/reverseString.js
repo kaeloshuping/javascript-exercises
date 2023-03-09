f6afc20bd178e4a2cb4a9f346b73b3134190fc56
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2023-03-09 17:20:33 +0200

    edit reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    stringArray = string.split("")
+    reverseArray = stringArray.reverse()
+    finalString = reverseArray.join("")
+    return finalString 
 };
 
 // Do not edit below this line
