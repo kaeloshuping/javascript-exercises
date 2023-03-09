c14bf185eac3d3095f439b06455d7d8b3fc737ff
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2023-03-09 19:08:42 +0200

    edit removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, toRemove1, toRemove2) {
+    let newArray = []
+    for (let i of array) {
+        if (i === toRemove1 || i === toRemove2) {
+            continue
+        }
+        else {
+            newArray.push(i)
+        };
+    };
+    return newArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+// def remove_item(list, item):
+//     new_list = []
+//     for i in list:
+//         if i == item:
+//             continue    
+//         else:
+//             new_list += i
