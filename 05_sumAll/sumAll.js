8c043736112384cbe9ad4108783332b2778829f5
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2023-03-14 19:26:46 +0200

    edit sumAll.spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    // create variable to hold final sum
+    let sum = 0
 
+    // get maximum value from "...args"
+    let maxNumber = Math.max(...args)
+
+    // get minimum value from "...args"
+    let minNumber = Math.min(...args)
+    console.log(maxNumber)
+    console.log(minNumber)
+    console.log(Number.isInteger(maxNumber))
+    if (Number.isInteger(minNumber) && Number.isInteger(maxNumber)) {
+        // loop through values between minNumber and maxNumber
+        for (let i = minNumber; i <= maxNumber; i++) {
+            // add numbers to sum
+            sum += i
+        }
+    }
+    else if (maxNumber < 0 || minNumber < 0) {
+        return "ERROR"
+    } 
+    else {
+        return "ERROR"
+    }
+    return sum
 };
 
 // Do not edit below this line
