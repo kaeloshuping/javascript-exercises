c2ee0ca66735e5a4d096c2591ebfcd391310ef06
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2023-03-27 14:02:49 +0200

    edit tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let celcius = (tempF - 32) * 5/9
+  let celciusFinal = celcius.toFixed(1)
+  return Number(celciusFinal)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let fahrenheit = (tempC * 9/5) + 32
+  let fahrenheitFinal = fahrenheit.toFixed(1)
+  return Number(fahrenheitFinal)
 };
 
 // Do not edit below this line
