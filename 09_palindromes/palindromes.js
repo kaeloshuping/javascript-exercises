3bec45df19c2cae099cdc5fce56b9895f95add72
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2023-06-08 16:28:45 +0200

    created function to detected palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    filteredString = string.toLowerCase().replace(/[^a-z]/g, "");
+    stringArray = filteredString.split("");
+    reversedArray = stringArray.reverse();
+    finalString = reversedArray.join("");
+    if (finalString === filteredString) {
+        return true;
+    }
+    else {
+        return false;
+    };
 };
 
 // Do not edit below this line
