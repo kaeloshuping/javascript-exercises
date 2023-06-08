a09f4a0c052fc1d689c3a3b1507aeac63b2925d4
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2023-06-08 14:33:55 +0200

    completed calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -3,24 +3,40 @@ const add = function(...args) {
   return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0
+  for (let i in array) {
+    sum += parseInt(array[i], 10);
+  };
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1
+  for (let i in array) {
+    product *= parseInt(array[i], 10);
+  };
+  return product;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number == 0) {
+    return 1;
+  }
+  else {
+    for (let i = number - 1; i > 0; i--) {
+      number *= i;
+    };
+    return number;
+  };
 };
 
 // Do not edit below this line
