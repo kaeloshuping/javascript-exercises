4b5112b0f72b25e1e0e52eed3a449c18f1bc430b
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2023-06-09 16:40:03 +0200

    completed fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) {
+        return "OOPS";
+    }
+    else {
+        let sequence = Array(number);
+        sequence.fill(0);
+        sequence[0] = 1;
+        sequence[1] = 1;
+        for (let i = 2; i < number; i++) {
+            sequence[i] = sequence[i - 1] + sequence[i - 2];
+        };
+        return sequence[number - 1];
+    };
 };
 
 // Do not edit below this line
