1728dd699a2a47ea65b37ec372da9735fc727de3
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2023-06-12 10:50:35 +0200

    created function to find the eldest in array of objects containing people birth years

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    ageArray = []
+    for (let person of array) {
+        if (person["yearOfDeath"] === undefined) {
+            currentDate = new Date()
+            currentYear = currentDate.getFullYear();
+            ageDifference = currentYear - person["yearOfBirth"];
+            ageArray.push(ageDifference);
+        }
+        else {
+            ageDifference = person["yearOfDeath"] - person["yearOfBirth"];
+            ageArray.push(ageDifference);
+        };
+    };
+    eldest = Math.max(...ageArray);
+    eldestIndex = ageArray.indexOf(eldest);
+    return array[eldestIndex]
 };
 
 // Do not edit below this line
